c92edd7b1eee34b1ae25f1056bfb2fef3f989c10
boris <boristondjua@gmail.com>
2026-03-23 15:52:02 +0100

    vieuw files adapt to use params in url as a filter

--- Projetc_QA_Auto/API_Testing/myproject/users/views.py
+++ Projetc_QA_Auto/API_Testing/myproject/users/views.py
@@ -9,3 +9,18 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+# This class help us to filter the Params of the url endpoint  in Postman
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        name = self.request.query_params.get('name')
+
+        if name:
+            queryset = queryset.filter(name=name)
+
+        return queryset
